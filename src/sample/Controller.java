a5967e24efd5c590d0a576b4cedc7a239b85607b
blagsov <blagsov@mail.ru>
2019-10-24 15:56:26 +0300

    изменила long на double, чтоб работать с дробными числами, добавила кнопки памяти (MS, MC, MR, M+, M-) и кнопки C и СE, а так же добавила панель с историей вычислений

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,20 +1,23 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 
-import java.awt.event.ActionEvent;
-
 public class Controller {
     @FXML
     private Text output;
-    private long num1 = 0;
-    // long num2 = 0;
+    @FXML
+    private Text memoryTextPanel;
+    private double num1 = 0;
+    private double num2 = 0;
 
     private boolean start = true;
     private String operator = "";
     private Model model = new Model();
+
+    private double memoryStore = 0;
 
 
     @FXML //для того, чтобы fxml мог обращаться к приватным методам
@@ -25,6 +28,7 @@ public class Controller {
         }
         String value = ((Button) event.getSource()).getText();//ispolzovat znachenie knopki, poluchit znachenie knopki
         output.setText(output.getText() + value);//k cifram, chto imeem dobavlyaem eche odnu
+        //   memoryTextPanel.setText(value);
     }
 
     @FXML
@@ -33,17 +37,61 @@ public class Controller {
         if (!"=".equals(value)) { //esli operacija ne ravno
             if (!operator.isEmpty()) return;//esli ne pustaja
             operator = value; //operator - znachenie
-            num1 = Long.parseLong(output.getText());
+            num1 = Double.parseDouble(output.getText());
+            //  memoryTextPanel.setText(String.valueOf(num1));
             output.setText("");//ochishaem
         } else {
             if (operator.isEmpty()) return;//prosto vyvoditsja znachenie, nichego ne proishodit
-            output.setText(String.valueOf(model.calculation(num1, Long.parseLong(output.getText()), operator)));//pervoe chislo,chislo posle operatora, operator
+            num2 = Long.parseLong(output.getText());
+            output.setText(String.valueOf(model.calculation(num1, num2, operator)));//pervoe chislo,chislo posle operatora, operator
+            memoryTextPanel.setText(memoryTextPanel.getText() + num1 + " " + operator + " " + num2 +
+                    " = " + model.calculation(num1, num2, operator) + "\n");
+            operator = "";
+            start = true;
+        }
+    }
+
+    @FXML
+    private void processMemory(ActionEvent event) {
+        String value = ((Button) event.getSource()).getText();//ispolzovat znachenie knopki, poluchit znachenie knopki
+
+        if (value.equals("MS")) {
+            memoryStore = Double.parseDouble(output.getText());
+            return;
+        }
+        if (value.equals("MC")) {
+            //Memory Clear
+            memoryStore = 0;
+            return;
+        }
+        if (value.equals("MR")) {
+            //Memory Recall
+            output.setText(String.valueOf(memoryStore));
+            return;
+        }
+        double endResult = 0;
+        if (value.equals("M-")) {
+            // Memory subtract
+            endResult = Double.parseDouble(output.getText());
+            memoryStore -= endResult;
+            return;
+        }
+        if (value.equals("M+")) {
+            // Memory add
+            endResult = Double.parseDouble(output.getText());
+            memoryStore += endResult;
+            return;
+        }
+        if (value.equals("C")) {
+            output.setText("");
             operator = "";
             start = true;
 
         }
-
-
+        if (value.equals("CE")) {
+            output.setText("");
+        }
     }
+
 
 }
--- src/sample/Model.java
+++ src/sample/Model.java
@@ -1,19 +1,27 @@
 package sample;
 
+import javafx.scene.control.Button;
+
 public class Model {
-    public long calculation(long a, long b, String operator){
-        switch (operator){ //proverjaem switchem calculjator
+
+    double endResult = 0;
+    double memoryStore = 0;
+    Button button = new Button();
+
+    public double calculation(double a, double b, String operator) {
+        switch (operator) { //proverjaem switchem calculjator
             case "+":
-                return a+b;
+                return a + b;
             case "-":
-                return a-b;
+                return a - b;
             case "*":
-                return a*b;
+                return a * b;
             case "/":
-                if (b==0)return 0;//chtoby ne delit na 0
-                return a/b;
+                if (b == 0) return 0;//chtoby ne delit na 0
+                return a / b;
         }
         System.out.println("Неизвестный оператор!");
         return 0;
     }
+
 }
